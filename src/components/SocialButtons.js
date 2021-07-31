1d73f50377f7d0073f6e8816d0f44d69e952ded0
h-sompura <sompharshi123@gmail.com>
2021-08-01 00:43:55 +0530

    Fixed noreferrer issue

--- src/components/SocialButtons.js
+++ src/components/SocialButtons.js
@@ -7,16 +7,16 @@ const SocialButtons = () => {
     return (
         <>
             <ButtonWrapper>
-                <Button><a target="_blank" href="mailto:sompharshi23@gmail.com"><HiOutlineMail style={{marginTop: '-.2em',marginRight:'.4em'}} size={22} /> Email</a></Button>
+                <Button><a target="_blank" without rel="noreferrer" href="mailto:sompharshi23@gmail.com"><HiOutlineMail style={{marginTop: '-.2em',marginRight:'.4em'}} size={22} /> Email</a></Button>
             </ButtonWrapper>
             <ButtonWrapper>
-                <Button><a target="_blank" href="https://www.linkedin.com/in/harshithasompura/"><AiFillLinkedin style={{marginTop: '-.2em',marginRight:'.3em'}} size={22} />Linkedin</a></Button>
+                <Button><a target="_blank" without rel="noreferrer" href="https://www.linkedin.com/in/harshithasompura/"><AiFillLinkedin style={{marginTop: '-.2em',marginRight:'.3em'}} size={22} />Linkedin</a></Button>
             </ButtonWrapper>
             <ButtonWrapper>
-                <Button><a target="_blank" href="https://github.com/h-sompura"><AiFillGithub style={{marginTop: '-.2em',marginRight:'.3em'}} size={22}/>Github </a></Button>
+                <Button><a target="_blank" without rel="noreferrer" href="https://github.com/h-sompura"><AiFillGithub style={{marginTop: '-.2em',marginRight:'.3em'}} size={22}/>Github </a></Button>
             </ButtonWrapper>
             <ButtonWrapper>
-                <Button><a target="_blank" href="https://dribbble.com/harshitha_sompura"><AiOutlineDribbble style={{marginTop: '-.2em',marginRight:'.3em'}} size={22}/>Dribbble </a></Button>
+                <Button><a target="_blank" without rel="noreferrer" href="https://dribbble.com/harshitha_sompura"><AiOutlineDribbble style={{marginTop: '-.2em',marginRight:'.3em'}} size={22}/>Dribbble </a></Button>
             </ButtonWrapper>
         </>
     )
